02e1dd8cbd2095a138af62fc42ebce9d45a031f7
jpittis <jakepittis@gmail.com>
2015-03-28 03:00:34 -0400

    added param to new player

--- src/world.js
+++ src/world.js
@@ -5,8 +5,8 @@ var player = require('./player');
 
 function World(tlSource, brSource) {
     this.map = new struct.Map(80, 50, 25); // goldenish ratio
-    this.tl = new player.Player("tl", tlSource, world);
-    this.br = new player.Player("br", brSource, world);
+    this.tl = new player.Player("tl", tlSource, map.tl, world);
+    this.br = new player.Player("br", brSource, map.br, world);
 
     this.step = function() {
         this.tl.step();
