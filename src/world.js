4177939114c2f788e60919f3e5cb25d9265493fa
Michael Layzell <michael@thelayzells.com>
2015-03-28 03:02:22 -0400

    add this to world

--- src/world.js
+++ src/world.js
@@ -5,13 +5,13 @@ var player = require('./player');
 
 function World(tlSource, brSource) {
     this.map = new struct.Map(80, 50, 25); // goldenish ratio
-    this.tl = new player.Player("tl", tlSource, map.tl, world);
-    this.br = new player.Player("br", brSource, map.br, world);
+    this.tl = new player.Player("tl", tlSource, this.map.tl, this);
+    this.br = new player.Player("br", brSource, this.map.br, this);
 
     this.step = function() {
         this.tl.step();
         this.br.step();
-    }
+    };
 }
 
 module.exports = {
