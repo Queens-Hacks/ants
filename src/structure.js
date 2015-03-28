fca0eb448d0ee0010e3a9147350231a91f30683b
Erik Pilkington <erik.pilkington@gmail.com>
2015-03-28 01:14:31 -0400

    wrote simple map generation function

--- src/structure.js
+++ src/structure.js
@@ -25,18 +25,40 @@ var Home = function(team) {
     }
 }
 
-function newMap(width, height) { // TODO
-
-}
-
-
-var map = new Array(20);
-for (var y = 0; y < 20; y++) {
-    map[y] = new Array(20);
-}
-
-for (var y = 0; y < 20; y++) {
-    for (var x = 0; x < 20; x++) {
-        map[y][x] = new Tile();
+function newMap(width, height, sugars) {
+    if(height < 10){
+        height = 10;
     }
+    if(width < 10){
+        width = 10;
+    }
+
+    var map = new Array(height);
+    for (var y = 0; y < height; y++) {
+        map[y] = new Array(width);
+    }
+    
+    for (var y = 0; y < width; y++) {
+        for (var x = 0; x < width; x++) {
+            map[y][x] = new Wall();
+        }
+    }
+    
+    for(var i = 0; i < sugars; i++) {
+        var x = Math.floor(Math.random() * height);
+        var y = Math.floor(Math.random() * width);
+        map[y][x] = new Sugar(10);
+    }
+
+    for(var i = 0; i < 5; i++) {
+        for(var j = 0; j < 5; j++) {
+            map[i][j] = new Empty();
+            map[height-i][width-j] = new Empty();
+        }
+    }
+
+    map[2][2] = new Home(1);
+    map[height-1][width-1] = new Home(2);
+
+    return map;
 }
