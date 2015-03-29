068e143be8e1b269fbc9c5ba1b452eeb5a16d86e
Michael Layzell <michael@thelayzells.com>
2015-03-29 03:11:30 -0400

    Switch to the new function-ific sandbox

--- src/player.js
+++ src/player.js
@@ -1,5 +1,6 @@
 var struct = require('./structure');
 var vector = require('./vector');
+var sketch = require('./sketch');
 var Vec2 = vector.Vec2;
 
 function debugLog(team, message) {
@@ -22,157 +23,117 @@ function Player(team, homeLocation, world) {
   this.world = world;
 
   this.setSource = function(source) {
-    var aether = this.aether = new Aether({
-      yieldAutomatically: true,
-      includeFlow: false,
-      includeMetrics: false
-    });
+    // try {
+      var compiled = sketch.compile(source, [
+        'dig',
+        'move',
+        'getTeam',
+        'log',
+        'sniff',
+        'spray',
+        'hasFood',
+        'home',
+        'location',
+        'goto',
+        'look',
+        'foodLeft'
+      ]);
+    // } catch (err) {
+    //   debugLog(team, "FATAL ERROR: " + err);
+    //   global.paused = true;
+    // }
 
-    aether.lint(source);
-    aether.problems.infos.forEach(function(info) {
-      debugLog(team, "INFO: " + info);
-    });
-    aether.problems.warnings.forEach(function(warning) {
-      debugLog(team, "WARN: " + warning);
-    });
-    aether.problems.errors.forEach(function(error) {
-      debugLog(team, "ERROR: " + error);
-    });
-
-    // TODO(michael): Check for any problems
-
-    try {
-      aether.transpile(source);
-      this.func = aether.createFunction();
-    } catch (err) {
-      debugLog(team, "FATAL ERROR: " + err);
-      global.paused = true;
-    }
-
-    var actionComplete = false;
     // Add an ant to the player object
     this.addAnt = function(location) {
       // Create the ant
       var ant = new struct.Ant(this.team, location, world);
       this.ants.push(ant);
 
-      var shim = Object.create(null);
-
-      shim.dig = function(direction) {
-        actionComplete = true;
-        return ant.dig(direction);
-      };
-
-      shim.move = function(direction) {
-        actionComplete = true;
-        if (! ant.move(direction)) {
-          return shim.dig(direction);
-        }
-        return true;
-      };
-
-      shim.getTeam = function() {
-        return team;
-      };
-
-      shim.log = function(message) {
-        debugLog(team, message);
-      };
-
-      // Checking or laying pheramones
-      // NOTE(michael): Temporarially not working
-      shim.sniff = function() {
-        return world.map.pherAt(team,
-                                new Vec2(ant.position.x, ant.position.y));
-      };
-      shim.spray = function(pheramone) {
-        world.map.setPherAt(team,
-                            new Vec2(ant.position.x, ant.position.y),
-                            // Ensure that the object is valid JSON
-                            JSON.parse(JSON.stringify(pheramone)));
-      };
-
-      shim.hasFood = function() {
-        return ant.hasFood;
-      };
-
-      shim.home = function() {
-        return homeLocation.clone();
-      };
-
-      shim.location = function() {
-        return ant.position.clone();
-      };
-
-      shim.goto = function(point) {
-        if (this.home().x > this.location().x) {
-            this.moveDig('right');
-        }
-        if (this.home().x < this.location().x) {
-            this.moveDig('left');
-        }
-        if (this.home().y > this.location().y) {
-            this.moveDig('down');
-        }
-        if (this.home().y < this.location().y) {
-            this.moveDig('up');
-        }
-      }
-
-      shim.look = function(direction) {
-        if (direction === 'up' && ant.position.y-1 > 0) {
-          return world.map.map[ant.position.y-1][ant.position.x].type;
-        } else if (direction === 'down' && ant.position.y+1 < world.map.map.length) {
-          return world.map.map[ant.position.y+1][ant.position.x].type;
-        } else if (direction === 'left' && ant.position.x-1 > 0) {
-          return world.map.map[ant.position.y][ant.position.x-1].type;
-        } else if (direction === 'right' && ant.position.x+1 < world.map.map[0].length) {
-          return world.map.map[ant.position.y][ant.position.x+1].type;
-        } else if (direction === 'here') {
-          return world.map.map[ant.position.y][ant.position.x].type;
-        } else {
-          return false;
-        }
-      };
-
-      shim.foodLeft = function(direction) {
-        if (direction === 'up' && ant.position.y-1 > 0) {
-          if (world.map.map[ant.position.y-1][ant.position.x].type === 'sugar') {
-            return world.map.map[ant.position.y-1][ant.position.x].amount;
-          } else {
-            return false;
+      ant.genState = new sketch.GenState(compiled, function(yv, yld, raw) {
+        var args = yv.args;
+        switch (yv.type) {
+        case 'dig':
+          return yld(ant.dig(args[0]));
+        case 'move':
+          if (! ant.move(args[0])) {
+            return yld(ant.dig(args[0]));
           }
-        } else if (direction === 'down' && ant.position.y+1 < world.map.map.length) {
-          if (world.map.map[ant.position.y+1][ant.position.x].type === 'sugar') {
-            return world.map.map[ant.position.y+1][ant.position.x].amount;
+          return yld(true);
+        case 'getTeam':
+          return raw(team);
+        case 'log':
+          debugLog(team, args.join(' '));
+          return raw();
+        case 'sniff':
+          return raw(world.map.pherAt(team,
+                                      new Vec2(ant.position.x, ant.position.y)));
+        case 'spray':
+          world.map.setPherAt(team,
+                              new Vec2(ant.position.x, ant.position.y),
+                              // Ensure that the object is valid JSON
+                              JSON.parse(JSON.stringify(args[0])));
+          return raw();
+        case 'hasFood':
+          return raw(ant.hasFood);
+        case 'home':
+          console.log('Home called', homeLocation.clone());
+          return raw(homeLocation.clone());
+        case 'location':
+          return raw(ant.position.clone());
+        case 'goto':
+          return yld();
+        case 'look':
+          if (args[0] === 'up' && ant.position.y-1 > 0) {
+            return raw(world.map.map[ant.position.y-1][ant.position.x].type);
+          } else if (args[0] === 'down' && ant.position.y+1 < world.map.map.length) {
+            return raw(world.map.map[ant.position.y+1][ant.position.x].type);
+          } else if (args[0] === 'left' && ant.position.x-1 > 0) {
+            return raw(world.map.map[ant.position.y][ant.position.x-1].type);
+          } else if (args[0] === 'right' && ant.position.x+1 < world.map.map[0].length) {
+            return raw(world.map.map[ant.position.y][ant.position.x+1].type);
+          } else if (args[0] === 'here') {
+            return raw(world.map.map[ant.position.y][ant.position.x].type);
           } else {
-            return false;
+            return raw(false);
           }
-        } else if (direction === 'left' && ant.position.x-1 > 0) {
-          if (world.map.map[ant.position.y][ant.position.x-1].type === 'sugar') {
-            return world.map.map[ant.position.y][ant.position.x-1].amount;
+        case 'foodLeft':
+          if (args[0] === 'up' && ant.position.y-1 > 0) {
+            if (world.map.map[ant.position.y-1][ant.position.x].type === 'sugar') {
+              return raw(world.map.map[ant.position.y-1][ant.position.x].amount);
+            } else {
+              return raw(false);
+            }
+          } else if (args[0] === 'down' && ant.position.y+1 < world.map.map.length) {
+            if (world.map.map[ant.position.y+1][ant.position.x].type === 'sugar') {
+              return raw(world.map.map[ant.position.y+1][ant.position.x].amount);
+            } else {
+              return raw(false);
+            }
+          } else if (args[0] === 'left' && ant.position.x-1 > 0) {
+            if (world.map.map[ant.position.y][ant.position.x-1].type === 'sugar') {
+              return raw(world.map.map[ant.position.y][ant.position.x-1].amount);
+            } else {
+              return raw(false);
+            }
+          } else if (args[0] === 'right' && ant.position.x+1 < world.map.map[0].length) {
+            if (world.map.map[ant.position.y][ant.position.x+1].type === 'sugar') {
+              return raw(world.map.map[ant.position.y][ant.position.x+1].amount);
+            } else {
+              return raw(false);
+            }
+          } else if (args[0] == 'here') {
+            if (world.map.map[ant.position.y][ant.position.x].type === 'sugar') {
+              return raw(world.map.map[ant.position.y][ant.position.x].amount);
+            } else {
+              return raw(false);
+            }
           } else {
-            return false;
+            return raw(false);
           }
-        } else if (direction === 'right' && ant.position.x+1 < world.map.map[0].length) {
-          if (world.map.map[ant.position.y][ant.position.x+1].type === 'sugar') {
-            return world.map.map[ant.position.y][ant.position.x+1].amount;
-          } else {
-            return false;
-          }
-        } else if (direction == 'here') {
-          if (world.map.map[ant.position.y][ant.position.x].type === 'sugar') {
-            return world.map.map[ant.position.y][ant.position.x].amount;
-          } else {
-            return false;
-          }
-        } else {
-          return false;
+        default:
+          throw new Error("Unrecognized yield function type: " + yv.type);
         }
-      }
-      // Start the script
-      var func = aether.createFunction();
-      ant.state = func.call(shim);
+      });
     };
 
     // Iterate over each of the ants, performing an action
@@ -181,45 +142,26 @@ function Player(team, homeLocation, world) {
     this.step = function() {
       this.ants.forEach(function(ant, index) {
         try {
-          actionComplete = false;
-          for (var i=0; i<500; i++) {
-            var next = ant.state.next();
-            if (next.done) { // Nuke the state object
-              ant.state = {
-                next: function() { return {}; }
-              };
-              break;
-            }
-            if (actionComplete) {
-              break;
-            }
-          }
+          // Run a single step
+          if (ant.dead) { return; }
+          var rslt = ant.genState.step();
+          if (rslt.done) { ant.dead = true; }
         } catch (err) {
           debugLog(team, "FATAL RT ERROR: " + err);
-          ant.state = {
-            next: function() { return {}; }
-          };
+          ant.dead = true;
         }
       });
 
       spawnCounter++;
-      var maxAnts = 5 + (world.map.getSugar(this.team)/2)
+      var maxAnts = 5 + (world.map.getSugar(this.team)/2);
       if (spawnCounter > 60 && this.ants.length < maxAnts) {
         this.addAnt(homeLocation.clone());
         spawnCounter = 0;
       }
-
-      // Ant adding
-      /* counter++;
-       if (counter > 5) {
-       console.log("Added an ant!");
-       this.addAnt(homeLocation);
-       counter = 0;
-       } */
     };
 
     this.addAnt(homeLocation.clone());
-  }
+  };
 }
 
 module.exports = {
---
+++ src/sketch.js
@@ -0,0 +1,183 @@
+// var esprima = require('esprima');
+var regenerator = require('regenerator');
+require('regenerator/runtime');
+var recast = require('recast');
+
+function compile(code, funcNames) {
+  // Parse the AST
+  var ast = recast.parse(code);
+  var b = recast.types.builders;
+
+  // Transform the AST nodes, making functions into generators, and
+  // function calls into yield expressions.
+  var visited = [];
+  recast.types.visit(ast, {
+    visitFunction: function(path) {
+      var node = path.node;
+      node.generator = true;
+
+      this.traverse(path);
+    },
+
+    visitCallExpression: function(path) {
+      // Prevent visiting the path twice
+      if (visited.indexOf(path.node) == -1) {
+        visited.push(path.node);
+      } else {
+        this.traverse(path);
+        return;
+      }
+
+      // Create the yield expression
+      var n = b.yieldExpression(path.node);
+      n = b.callExpression(b.identifier("____id_hack_this_sucks"), [n]);
+
+      console.log(n);
+
+      path.replace(n);
+
+      // Continue processing
+      this.traverse(path);
+    }
+  });
+
+  // Wrap in a generator
+  var wrapped = "function* ____main____(" + funcNames.join(', ') + ") {\n" + recast.print(ast).code + "\n}";
+
+  console.log(wrapped);
+
+  // And compile to es5 with regenerator
+  var es5Src = regenerator.compile(wrapped, { includeRuntime: false }).code;
+
+  console.log(es5Src);
+
+  // Regenerator has a bug where property accesses on yield statements
+  // are horribly broken. This is an awful hack to get around that bug
+  var idHack = "\nfunction ____id_hack_this_sucks(x) { return x; };\n";
+
+  // Create the function object
+  var func = new Function(idHack + es5Src + "; return ____main____;");
+  var yieldActions = funcNames.map(function(name) {
+    return function() {
+      var args = new Array(arguments.length);
+      for (var i=0; i<arguments.length; i++) {
+        args[i] = arguments[i];
+      }
+
+      return { ___yield_action___: true, type: name, args: args };
+    };
+  });
+
+  // Produce a wrapped function
+  return function() { return func().apply(null, yieldActions); };
+}
+
+// Create a GenState object at the starting state
+function GenState(func, handler) {
+  this.gens = [func()];
+  console.log(this.gens);
+  this.handler = handler;
+  this.nextValue = undefined;
+}
+
+// run next on the top item in the stack
+GenState.prototype._next = function(value) {
+  return this.gens[this.gens.length - 1].next(value);
+};
+
+GenState.prototype.step = function() {
+  // Run the generator once
+  var yvw = this._next(this.nextValue);
+
+  while (true) {
+    // console.log("yvw =", yvw);
+
+    var yv = yvw.value;
+    if (yvw.done) {
+      // console.log("done");
+      // We're done a layer, go back up
+      this.gens.pop();
+      if (this.gens.length <= 0) {
+        // We were at the top, we're done!
+        return { done: true, value: yv };
+      }
+      // Run the next action
+      yvw = this._next(yv);
+      // yvw = genstate.gens[genstate.gens.length - 1].next(yv);
+    } else if (yv &&
+               Object.hasOwnProperty.call(yv, '___yield_action___') &&
+               yv.___yield_action___) {
+      // Run the handler
+      // console.log("yieldaction");
+      var hresult = this.handler(yv, function(v) {
+        return { yld: true, val: v };
+      }, function(v) {
+        return { yld: false, val: v };
+      });
+
+      if (hresult.yld) {
+        // It was a yield action! Stop running
+        this.nextValue = hresult.val;
+        return { done: false };
+      } else {
+        yvw = this._next(hresult.val);
+      }
+    } else if (yv &&
+               typeof(yv.next) === 'function') {
+      // We're looking at a generator
+      // console.log("generator");
+
+      this.gens.push(yv);
+      yvw = this._next(undefined);
+    } else {
+      // It's a raw value, don't do much other than just send it back in
+      // console.log("value");
+      yvw = this._next(yv);
+    }
+  }
+};
+
+module.exports = {
+  compile: compile,
+  GenState: GenState
+};
+
+
+/*
+var compiled = compile(source, [
+  'walk',
+  'run',
+  'eat',
+  'swim'
+]);
+var genState = new GenState(compiled, function(yv) {
+  switch (yv.type) {
+  case 'walk':
+    console.log("Walking...");
+    return true;
+    break;
+  case 'run':
+    console.log("Running...");
+    return false;
+    break;
+  case 'eat':
+    console.log("Eating...");
+    return true;
+    break;
+  case 'swim':
+    console.log("Swimming...");
+    return false;
+    break;
+  default:
+    throw new Error("Unrecognized yield type");
+  }
+});
+
+while (true) {
+  var rslt = genState.step();
+  console.log("~~~ Stepped ~~~");
+  if (rslt.done) break;
+}
+
+console.log("Done");
+  */
