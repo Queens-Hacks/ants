7ecf3d3b1500190e576439b0f9fa01556176d00e
Erik Pilkington <erik.pilkington@gmail.com>
2015-03-28 20:20:39 -0400

    what even

--- src/world.js
+++ src/world.js
@@ -3,14 +3,14 @@ var vector = require('./vector');
 var player = require('./player');
 
 function World() {
-    this.map = new struct.Map(80, 50, 25); // goldenish ratio
+    this.map = new struct.Map(80, 50, 12); // goldenish ratio
     this.tl = new player.Player("tl", this.map.tl, this);
     this.br = new player.Player("br", this.map.br, this);
     this.step = function() {
         this.tl.step();
         this.br.step();
-        if (this.map.getSugar('tl') > 100) alert("team left wins!");
-        if (this.map.getSugar('br') > 100) alert("team right wins!");
+        if (this.map.getSugar('tl') > 50) alert("team left wins!");
+        if (this.map.getSugar('br') > 50) alert("team right wins!");
         
     };
     this.setSources = function(tlSource, brSource) {
