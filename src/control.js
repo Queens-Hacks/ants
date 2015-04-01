5ab269b1591abe96db45a954e4a6dc474b8b4b95
jpittis <jakepittis@gmail.com>
2015-03-31 21:27:42 -0400

    added control and refactored main into control

---
+++ src/control.js
@@ -0,0 +1,83 @@
+var world = require('./world');
+
+var display;
+var tc, tlcount, brcount, tickNum;
+var outputleft, ouputright;
+var inputleft, inputright;
+var sourceSupplied, gameWorld, winner;
+
+function init(options) {
+    display = options.display;
+    tc = options.tlcount;
+    tlcount = options.tlcount;
+    brcount = options.brcount;
+    tickNum = options.tickNum;
+    outputleft = options.outputleft;
+    outputright = options.ouputright;
+    inputleft = options.inputleft;
+    inputright = options.inputright;
+    sourceSupplied = options.sourceSupplied;
+    gameWorld = options.gameWorld;
+    winner = options.winner;
+}
+
+function pause() {
+    var imageTag = document.querySelector("#play-pause > img");
+    if (paused) {
+        if (winner != 0) {
+            return;
+        }
+        imageTag.setAttribute("src", "pause.png");
+        imageTag.setAttribute("alt", "pause");
+        paused = false;
+        if (!sourceSupplied) {
+            gameWorld.setSources(inputleft.getValue(), inputright.getValue());
+            sourceSupplied = true;
+        }
+        run();
+    } else {
+        paused = true;
+        imageTag.setAttribute("src", "play.png");
+        imageTag.setAttribute("alt", "play");
+    }
+}
+
+function restart() {
+    winner = 0;
+    sourceSupplied = false;
+    gameWorld = new world.World();
+    paused = true;
+    tickNum = 0;
+    tc.textContent = "t = " + tickNum;
+    tlcount.textContent = gameWorld.map.getSugar('tl');
+    brcount.textContent = gameWorld.map.getSugar('br');
+    outputleft.setValue("~~ Pink Program Log ~~");
+    outputright.setValue("~~ Blue Program Log ~~");
+    display.render(gameWorld);
+}
+
+var requestAnimFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(callback) {
+        window.setTimeout(callback, 100);
+};
+
+function run() {
+    if (paused) {
+        return;
+    }
+    if (winner === 0) {
+        winner = gameWorld.step();
+        tickNum++;
+    }
+    display.render(gameWorld, winner);
+    tc.textContent = "t = " + tickNum;
+    tlcount.textContent = gameWorld.map.getSugar('tl');
+    brcount.textContent = gameWorld.map.getSugar('br');
+    requestAnimFrame(run);
+}
+
+module.exports = {
+    restart: restart,
+    pause: pause,
+    run: run,
+    init: init
+};
--- src/main.js
+++ src/main.js
@@ -1,91 +1,60 @@
-var world = require('./world');
+var World = require('./world');
+var Control = require('./control');
 var Display = require('./graphics');
-var display = new Display(document.getElementById("canvas"));
-// Get the elements
-var tc = document.getElementById("tickCounter");
-var tlcount = document.getElementById("tlCount");
-var brcount = document.getElementById("brCount");
-var tickNum = 0;
-// Helper function
-var requestAnimFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(callback) {
-        window.setTimeout(callback, 100);
-    };
-// Create the ace editors
-var inputleft = ace.edit("inputleft");
-inputleft.setTheme("ace/theme/monokai");
-inputleft.getSession().setMode("ace/mode/javascript");
-var inputright = ace.edit("inputright");
-inputright.setTheme("ace/theme/monokai");
-inputright.getSession().setMode("ace/mode/javascript");
-var outputleft = ace.edit("logboxleft");
-outputleft.setTheme("ace/theme/monokai");
-outputleft.setReadOnly(true);
-global.outputleft = outputleft;
-var outputright = ace.edit("logboxright");
-outputright.setTheme("ace/theme/monokai");
-outputright.setReadOnly(true);
-global.outputright = outputright;
-global.paused = true;
-var sourceSupplied = false;
-var gameWorld = new world.World();
-var winner = 0;
 
-function run() {
-    if (paused) {
-        return;
-    }
-    if (winner === 0) {
-        winner = gameWorld.step();
-        tickNum++;
-    }
-    display.render(gameWorld, winner);
-    tc.textContent = "t = " + tickNum;
-    tlcount.textContent = gameWorld.map.getSugar('tl');
-    brcount.textContent = gameWorld.map.getSugar('br');
-    requestAnimFrame(run);
-}
+var controlOptions = {
+    display: new Display(document.getElementById("canvas")),
+
+    tc: document.getElementById("tickCounter"),
+    tlcount: document.getElementById("tlCount"),
+    brcount: document.getElementById("brCount"),
+    tickNum: 0,
+    
+    outputleft: ace.edit("logboxleft"),
+    outputright: ace.edit("logboxright"),
+
+    // create the ace editors
+    inputleft: ace.edit("inputleft"),
+    inputright: ace.edit("inputright"),
+
+    sourceSupplied: false,
+    gameWorld: new World.World(),
+    winner: 0
+};
+
+Control.init(controlOptions);
+
+global.paused = true; // TODO export by control
+
+controlOptions.outputleft.setTheme("ace/theme/monokai");
+controlOptions.outputleft.setReadOnly(true);
+global.outputleft = controlOptions.outputleft; // TODO export by control
+
+controlOptions.outputright.setTheme("ace/theme/monokai");
+controlOptions.outputright.setReadOnly(true);
+global.outputright = controlOptions.outputright; // TODO export by control
+
+controlOptions.inputleft.setTheme("ace/theme/monokai");
+controlOptions.inputleft.getSession().setMode("ace/mode/javascript");
+
+controlOptions.inputright.setTheme("ace/theme/monokai");
+controlOptions.inputright.getSession().setMode("ace/mode/javascript");
+
+controlOptions.display.render(controlOptions.gameWorld);
+controlOptions.outputleft.setValue("~~ Pink Program Log ~~");
+controlOptions.outputright.setValue("~~ Blue Program Log ~~");
+
 var pausebtn = document.getElementById("play-pause");
 pausebtn.addEventListener('click', function(e) {
-    var imageTag = document.querySelector("#play-pause > img");
-    if (paused) {
-        if (winner != 0) {
-            return;
-        }
-        imageTag.setAttribute("src", "pause.png");
-        imageTag.setAttribute("alt", "pause");
-        paused = false;
-        e.preventDefault();
-        if (!sourceSupplied) {
-            gameWorld.setSources(inputleft.getValue(), inputright.getValue());
-            sourceSupplied = true;
-        }
-        run();
-    } else {
-        paused = true;
-        imageTag.setAttribute("src", "play.png");
-        imageTag.setAttribute("alt", "play");
-    }
     e.preventDefault();
+    Control.pause();
 });
+
 var restartbtn = document.getElementById("restart");
 restartbtn.addEventListener('click', function(e) {
-    winner = 0;
     e.preventDefault();
-    sourceSupplied = false;
-    gameWorld = new world.World();
-    paused = true;
-    tickNum = 0;
-    tc.textContent = "t = " + tickNum;
-    tlcount.textContent = gameWorld.map.getSugar('tl');
-    brcount.textContent = gameWorld.map.getSugar('br');
-    outputleft.setValue("~~ Pink Program Log ~~");
-    outputright.setValue("~~ Blue Program Log ~~");
-    display.render(gameWorld);
+    Control.restart();
 });
-display.render(gameWorld);
-outputleft.setValue("~~ Pink Program Log ~~");
-outputright.setValue("~~ Blue Program Log ~~");
-
 
 var fileuploadleft = document.getElementById("fileuploadleft");
 fileuploadleft.addEventListener('change', function(e) {
@@ -93,7 +62,7 @@ fileuploadleft.addEventListener('change', function(e) {
 
   var reader = new FileReader();
   reader.onload = function(e) {
-    inputleft.setValue(e.target.result);
+    controlOptions.inputleft.setValue(e.target.result);
   };
 
   reader.readAsText(files[0]);
@@ -105,7 +74,7 @@ fileuploadright.addEventListener('change', function(e) {
 
   var reader = new FileReader();
   reader.onload = function(e) {
-    inputright.setValue(e.target.result);
+    controlOptions.inputright.setValue(e.target.result);
   };
 
   reader.readAsText(files[0]);
